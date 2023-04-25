132c5be40b48a1e90e3f7fc4009cdcd9ca2f5999
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-04-25 16:10:15 -0700

    BOOLEAN LOGIC SUBMISSION ASSIGNMENT

---
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. What is your age?\n");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Have you ever had a DUI? Please type \"yes\" or \"no\"\n");
+            string dui = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("How many speeding tickets do you have?\n");
+            string ticket = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            int yourAge = Convert.ToInt32(age);
+            int speedingTickets = Convert.ToInt32(ticket);
+           
+            Console.WriteLine("Qualified?\n");
+            bool qualified = (yourAge > 15 && dui == "no" && speedingTickets < 4);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+
+
+        }
+        
+    }
+}
