5e4421bb05baab6bd2b143715f6d305a1bef4fe2
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-04-30 12:15:13 -0700

    CONSOLE APP ASSIGNMENT - RESUBMIT

--- Basic_C#_Programs/ConsoleAppAssignment-6parts/ConsoleAppAssignment-6parts/Program.cs
+++ Basic_C#_Programs/ConsoleAppAssignment-6parts/ConsoleAppAssignment-6parts/Program.cs
@@ -6,100 +6,97 @@ class Program
     static void Main(string[] args)
     {
         //PART 1
-        //string[] fruits = new string[5];
-        //Console.WriteLine("Please add fruits to the array.");
+        string[] fruits = new string[] { "apple\n", "banana\n", "grapes\n" };
+        Console.WriteLine("Please add fruits to the array.");
 
-        //for (int i = 0; i < fruits.Length; i++)
-        //{
-        //    string fruitsAdd = Console.ReadLine();
-        //    fruits[i] += fruitsAdd;
-        //}
-        //Console.WriteLine("\n");
-        //foreach (string fruit in fruits)
-        //{
-        //    Console.WriteLine(fruit);
-        //}
-        //Console.ReadLine();
+        for (int i = 0; i < fruits.Length; i++)
+        {
+            string fruitsAdd = Console.ReadLine();
+            fruits[i] += fruitsAdd;
+        }
+        Console.WriteLine("\n");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit + "\n");
+        }
+        Console.ReadLine();
 
-        //PART 2
-        //for (; ; ) 
-        //{
-        //    Console.WriteLine("Infinite Loop");
-        //}
-
-        //for (; ; ) 
+     //PART 2
+        //for (; ; )
         //{
         //    Console.WriteLine("Infinite Loop");
-        //    break;
         //}
 
-        //PART 3
-        //int[] lessThan = new int[] { 16, 12, 3, 56, 42, 88, 97, 61};
-        //foreach (int num in lessThan)
-        //{
-        //    if (num < 97)
-        //    {
-        //        Console.WriteLine(num);
-        //    }
-        //}
-        //Console.ReadLine();
+        for (; ; )
+        {
+            Console.WriteLine("Infinite Loop");
+            break;
+        }
 
-        //foreach (int num in lessThan)
-        //{
-        //    if (num <= 56)
-        //    {
-        //        Console.WriteLine(num);
-        //    }
-        //}
-        //Console.ReadLine();
+     //PART 3
+        int[] lessThan = new int[] { 16, 12, 3, 56, 42, 88, 97, 61 };
+        for (int n = 0; n < lessThan.Length; n++)
+        {
+           Console.WriteLine(n);
+       
+        }
+        Console.ReadLine();
 
-        //PART 4
-        //List<string> colors = new List<string>() { "red", "yellow", "blue" };
+        for (int n = 0; n <= lessThan.Length; n++)
+        {
+            Console.WriteLine(n);
 
-        //Console.WriteLine("Please input a color to search for in the list.");
-        //string chosenColor = Console.ReadLine();
+        }
+        Console.ReadLine();
 
+     //PART 4
+        List<string> colors = new List<string>() { "red", "yellow", "blue" };
 
-        //if (colors.Contains(chosenColor))
-        //{
-        //    for (int c = 0; c < colors.Count; c++)
-        //    {
-        //        if (colors[c] == chosenColor)
-        //        {
-        //            Console.WriteLine("That color is at index: " + c);
-        //        }
-        //    }
-        //}
-        //else
-        //{
-        //    Console.WriteLine("That color isn't in the list, try again.");
-        //}
-        //Console.ReadLine();
-
-        //PART 5
-        //List<string> animals = new List<string>() { "dog", "cat", "lizard", "cow", "hamster", "bird", "lion", "bear" };
-
-        //Console.WriteLine("Please input a color to search for in the list.");
-        //string chosenAnimal = Console.ReadLine();
+        Console.WriteLine("Please input a color to search for in the list.");
+        string chosenColor = Console.ReadLine();
 
 
-        //if (animals.Contains(chosenAnimal))
-        //{
-        //    for (int a = 0; a < animals.Count; a++)
-        //    {
-        //        if (animals[a] == chosenAnimal)
-        //        {
-        //            Console.WriteLine("That color is at index: " + a);
-        //        }
-        //    }
-        //}
-        //else
-        //{
-        //    Console.WriteLine("That animal isn't in the list, try again.");
-        //}
-        //Console.ReadLine();
+        if (colors.Contains(chosenColor))
+        {
+            for (int c = 0; c < colors.Count; c++)
+            {
+                if (colors[c] == chosenColor)
+                {
+                    Console.WriteLine("That color is at index: " + c);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("That color isn't in the list, try again.");
+        }
+        Console.ReadLine();
 
-        //PART 6
+     //PART 5
+        List<string> animals = new List<string>() { "dog", "cat", "lizard", "cow", "hamster", "bird", "cow", "bear" };
+
+        Console.WriteLine("Please input a color to search for in the list.");
+        string chosenAnimal = Console.ReadLine();
+
+
+        if (animals.Contains(chosenAnimal))
+        {
+            for (int a = 0; a < animals.Count; a++)
+            {
+                if (animals[a] == chosenAnimal)
+                {
+                    Console.WriteLine("That animal is at index: " + a);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("That animal isn't in the list, try again.");
+        }
+        Console.ReadLine();
+
+     //PART 6
         List<string> names = new List<string>() { "zella", "gabe", "vlad", "ripper", "axel", "zella" };
         List<string> checkName = new List<string>();        
 
