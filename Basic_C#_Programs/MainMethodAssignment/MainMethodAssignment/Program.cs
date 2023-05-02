269ae01198e555561cc66d3344e8ef8fa72b8bd8
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-05-01 17:31:41 -0700

    MAIN METHOD SUBMISSION ASSIGNMENT

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            integerMath mainMethod = new integerMath();
+
+            Console.WriteLine("Please enter a number");
+            string userNumber = Console.ReadLine();
+            int userNum = Convert.ToInt32(userNumber);
+
+            Console.WriteLine(userNum + " minus 5 equals " + mainMethod.mathOp(userNum));
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a decimal");
+            string userDecimal = Console.ReadLine();
+            decimal userDec = (decimal)Convert.ToDouble(userDecimal);
+            Console.WriteLine(userDec + " minus 1 equals " + mainMethod.mathOp(userDec));
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a number");
+            string userString = Console.ReadLine();
+
+            Console.WriteLine(userString + " divided by 2 equals " + Convert.ToInt32(mainMethod.mathOp(userString)));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/integerMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class integerMath
+    {
+        public int mathOp(int value)
+        {
+            return value - 5;
+        }
+        public decimal mathOp(decimal value)
+        {
+            return value - 1;
+        }
+        public string mathOp(string value)
+        {
+            int intValue = Convert.ToInt32(value);
+            intValue = intValue / 2;
+            string stringValue = Convert.ToString(intValue);
+            return stringValue;
+        }
+    }
+}
