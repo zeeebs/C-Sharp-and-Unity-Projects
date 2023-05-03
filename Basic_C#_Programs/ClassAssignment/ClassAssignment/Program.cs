793402d4f1630d85e3cbb7508e0fceb1fae19c86
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-05-02 19:23:31 -0700

    CLASS SUBMISSION ASSIGNMENT

---
+++ Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            voidClass voidMethod = new voidClass();
+
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            voidClass.intOutput(userInput);
+            Console.ReadLine();
+
+
+            voidClass OutPar = new voidClass();
+
+            voidClass.outputParameters(out int value1, out int value2);
+            {
+                Console.WriteLine(value1);
+                Console.WriteLine(value2);
+                Console.ReadLine();
+            }
+            voidClass.outputParameters(out string value4, out string value5);
+            {
+                Console.WriteLine(value4);
+                Console.WriteLine(value5);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+            
+    }
+}
+
---
+++ Basic_C#_Programs/ClassAssignment/ClassAssignment/voidClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class voidClass
+    {
+        public static void intOutput(int userInput)
+        {
+            int quotient = userInput / 2;
+            Console.WriteLine(quotient);
+        }
+
+        public static void outputParameters(out int value1, out int value2)
+        {
+            int value3 = 5;
+            value2 = value3;
+            value1 = value2 + value3;
+        }
+
+        public static void outputParameters(out string value4, out string value5)
+        {
+            string value3 = "banana";
+            value5 = value3;
+            value4 = value5;
+        }
+    }
+}
